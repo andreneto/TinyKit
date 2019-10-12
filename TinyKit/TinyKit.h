9f360760c8944019c65d427d70a330392be6f363
Andre Carneiro <jacn@cin.ufpe.br>
2019-10-12 11:19:19 -0300

    Initial Commit

---
+++ TinyKit/TinyKit.h
@@ -0,0 +1,19 @@
+//
+//  TinyKit.h
+//  TinyKit
+//
+//  Created by André Carneiro on 12/10/19.
+//  Copyright © 2019 André Carneiro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TinyKit.
+FOUNDATION_EXPORT double TinyKitVersionNumber;
+
+//! Project version string for TinyKit.
+FOUNDATION_EXPORT const unsigned char TinyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TinyKit/PublicHeader.h>
+
+
